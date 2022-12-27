6853960cbcd5997dfa2e71e1de0d547aae5f955b
Newton Lomar <newtonlomar16@gmail.com>
2022-12-27 14:04:57 -0300

    adding static files, static files midleware and webpack config

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const routes = require("./routes");
 const path = require("path");
 
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
